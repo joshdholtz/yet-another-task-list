986062580f9c9fb3326e7ca49170f59bb47549d4
Josh Holtz <josh@rokkincat.com>
2019-01-26 15:54:21 -0600

    It works

---
+++ precache-manifest.6596045bd8d6a7a608de21b57d0058fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b86296829065f4e66b1f",
+    "url": "/static/js/main.b8629682.chunk.js"
+  },
+  {
+    "revision": "a718d54cf6f6ff1d9752",
+    "url": "/static/js/1.a718d54c.chunk.js"
+  },
+  {
+    "revision": "b86296829065f4e66b1f",
+    "url": "/static/css/main.8e602c8e.chunk.css"
+  },
+  {
+    "revision": "a718d54cf6f6ff1d9752",
+    "url": "/static/css/1.c716932c.chunk.css"
+  },
+  {
+    "revision": "38364d5c4dcab31e7a81038434fd5ebf",
+    "url": "/index.html"
+  }
+];
